75a44448c2fbc55a582193862a1e4d0ad2de37ab
Dreamphpr <1095421013@qq.com>
2018-11-05 15:04:05 +0800

    用户编辑帖子及权限

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -42,12 +42,12 @@ class TopicsController extends Controller
         return redirect()->route('topics.show', $topic->id)->with('message', '成功创建话题！');
     }
 
-	public function edit(Topic $topic)
-	{
+    public function edit(Topic $topic)
+    {
         $this->authorize('update', $topic);
-		return view('topics.create_and_edit', compact('topic'));
-	}
-
+        $categories = Category::all();
+        return view('topics.create_and_edit', compact('topic', 'categories'));
+    }
 	public function update(TopicRequest $request, Topic $topic)
 	{
 		$this->authorize('update', $topic);
